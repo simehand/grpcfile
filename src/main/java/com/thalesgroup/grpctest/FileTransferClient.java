60dc9ecdc64f52d326678516c0098891d231a4e4
asimehand <a.simehand@email.com>
2024-11-19 23:25:27 +0100

    Changes

--- src/main/java/com/thalesgroup/grpctest/FileTransferClient.java
+++ src/main/java/com/thalesgroup/grpctest/FileTransferClient.java
@@ -18,6 +18,7 @@ import com.thalesgroup.grpctest.Siafiletransfer.FileChunckRequest;
 import com.thalesgroup.grpctest.Siafiletransfer.FileChunckResponse;
 
 import io.grpc.Channel;
+import io.grpc.ConnectivityState;
 import io.grpc.LoadBalancerRegistry;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -37,86 +38,93 @@ public class FileTransferClient {
     public FileTransferClient(Channel channel) {
         blockingStub = FiletTransferGrpc.newBlockingStub(channel);
         stub = FiletTransferGrpc.newStub(channel);
+
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        /// Enter data using BufferReader
-
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
-        System.out.println("\n Enter remote server, format: <host:port>");
-        // Reading server
-        String server = reader.readLine();
-
-        System.out.println("\n Enter  file path");
-        // Reading file path
-        String filePath = reader.readLine();
-
-        File file = new File(filePath);
-        if (!file.exists())
-           System.out.println("\n  File path : "+filePath+" is not correct");
-
-        System.out.println("\n Do you want to send streams (Y or N)");
-        // Reading file path
-        String sendStream = reader.readLine();
-
-        // Printing the read line
-         System.out.println("Server address: "+server +  ", file path"+ filePath);
-
-        //String filePath = "/Users/asimehand/Documents/grpcfile/files/docs.pdf";
-        //String server = "127.0.0.1:9001";
-        //String sendStream = "Y";
-        ManagedChannel channel = ManagedChannelBuilder.forTarget(server)
-                .usePlaintext()
-                .maxInboundMessageSize(Integer.MAX_VALUE)
-                .build();
-
         try {
-            
+            /// Enter data using BufferReader
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+            System.out.println("\n Enter remote server, format: <host:port> : ");
+            // Reading server
+            String server = reader.readLine();
+            ManagedChannel channel = ManagedChannelBuilder.forTarget(server)
+                    .usePlaintext()
+                    .maxInboundMessageSize(Integer.MAX_VALUE)
+                    .build();
 
             FileTransferClient client = new FileTransferClient(channel);
+
+            System.out.println("\n Enter  file path :");
+            // Reading file path
+            String filePath = reader.readLine();
+
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.out.println("\n  File path : " + filePath + "  doesn't exist, enter a valid path :");
+                return;
+            }
+
+            System.out.println("\n Do you want to send streams (Y or N) :");
+            // Reading file path
+            String sendStream = reader.readLine();
+
+            // Printing the read line
+            System.out.println("Server address: " + server + ", file path" + filePath);
+
+            // String filePath = "/Users/asimehand/Documents/grpcfile/files/docs.pdf";
+            // String server = "127.0.0.1:9001";
+            // String sendStream = "Y";
+
             if (sendStream.equals("Y"))
                 client.sendStream(file.getAbsolutePath(), file.getName());
             else
                 client.sendUnary(file.getAbsolutePath(), file.getName());
-        } finally {
-            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
 
     }
 
     // Normal Unary request/response
     public void sendUnary(String filePath, String fileName) throws IOException, InterruptedException {
-        LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
-        // read the file and convert to a byte array
-        InputStream inputStream = new FileInputStream(filePath);
+        try {
 
-        byte[] bytes = inputStream.readAllBytes();
-        int length = bytes.length;
+            LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
+            // read the file and convert to a byte array
+            InputStream inputStream = new FileInputStream(filePath);
 
-        FileChunckRequest req = FileChunckRequest.newBuilder()
-                .setData(ByteString.copyFrom(bytes, 0, length))
-                .setOffset(0)
-                .setFilename(fileName)
-                .setSize(length)
-                .setOffset(0)
-                .build();
-        millis = System.currentTimeMillis() % 1000;
-        logger.info("\n Start uploading .......");
-        FileChunckResponse resp = blockingStub.withCompression("gzip").fullUplaod(req);
-        logger.log(Level.INFO, "Ack received: {0}", resp.getComplete());
-        if (resp.getComplete()) {
-            logger.info("Completed upload in " + (millis - (System.currentTimeMillis() % 1000)) + "ms");
+            byte[] bytes = inputStream.readAllBytes();
+            int length = bytes.length;
+
+            FileChunckRequest req = FileChunckRequest.newBuilder()
+                    .setData(ByteString.copyFrom(bytes, 0, length))
+                    .setOffset(0)
+                    .setFilename(fileName)
+                    .setSize(length)
+                    .setOffset(0)
+                    .build();
+            millis = System.currentTimeMillis() % 1000;
+            logger.info("\n Start uploading .......");
+            FileChunckResponse resp = blockingStub.withCompression("gzip").fullUplaod(req);
+            logger.log(Level.INFO, "Ack received: {0}", resp.getComplete());
+            if (resp.getComplete()) {
+                logger.info("Completed upload in " + (millis - (System.currentTimeMillis() % 1000)) + "ms");
+            }
+
+            inputStream.close();
+            millis = 0;
+        } finally {
+            ((ManagedChannel) stub.getChannel()).shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
-
-        inputStream.close();
-        millis = 0;
 
     }
 
     // Send streams
-    public void sendStream(String filePath, String fileName) {
+    public void sendStream(String filePath, String fileName) throws InterruptedException {
 
         LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
 
@@ -166,6 +174,8 @@ public class FileTransferClient {
 
         } catch (Throwable t) {
             logger.log(Level.SEVERE, filePath, t);
+        } finally {
+            ((ManagedChannel) stub.getChannel()).shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
 
     }
